ae63cafc8d58592d19ae47293b4c85d35866b127
salodoxera <DavidDubovik19@gmail.com>
2025-01-07 19:57:58 +0200

    fix 0.8

--- task.sql
+++ task.sql
@@ -11,28 +11,28 @@ CREATE TABLE Countries (
 -- Create a table for caching GeoIP data (Columns: ID, IP Range, CountryID)
 
 CREATE TABLE GeoIPCache (
-    ID INT NOT NULL,
+    ID INT,
     IPRange VARCHAR(100),
-    CountryID INT NOT NULL,
+    CountryID INT,
     PRIMARY KEY (ID)
-) ENGINE=InnoDB;
+) ENGINE=MEMORY;
 
 -- Create a table for storing product descriptions for different countries (Columns: ID, CountryID, ProductID, Description )
 CREATE TABLE ProductDescription (
-    ID INT NOT NULL,
+    ID INT,
     Description TEXT,
-    ProductID INT NOT NULL,
-    CountryID INT NOT NULL,
+    ProductID INT,
+    CountryID INT,
     PRIMARY KEY (ID)
 ) ENGINE=InnoDB;
 
 -- Create a table for storing logs. For now we don't need to save them, but we need to implement functionality (Columns: ID, Time, LogRecord)
 CREATE TABLE Logs (
-    ID INT NOT NULL,
+    ID INT,
     Timestamp DATETIME,
     Message TEXT,
     PRIMARY KEY (ID)
-) ENGINE=InnoDB;
+) ENGINE=BLACKHOLE ;
 -- Create a table for storing reporting data, which will be send to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
 CREATE TABLE ProductReporting (
     Date DATE,
