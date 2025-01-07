94d0b35ab0bdedcc21a00de8ab07e00ce2b5d3fb
salodoxera <DavidDubovik19@gmail.com>
2025-01-07 19:29:03 +0200

    Solution

--- task.sql
+++ task.sql
@@ -10,8 +10,33 @@ CREATE TABLE Countries (
 
 -- Create a table for caching GeoIP data (Columns: ID, IP Range, CountryID)
 
+CREATE TABLE GeoIPCache (
+    ID INT,
+    IPRangeі  INT,
+    CountryID INT,
+    PRIMARY KEY (ID)
+) ENGINE=GeoIPCache;
+
 -- Create a table for storing product descriptions for different countries (Columns: ID, CountryID, ProductID, Description )
+CREATE TABLE ProductDescription (
+    ID INT,
+    Description VARCHAR(255),
+    ProductID INT,
+    CountryID INT,
+    PRIMARY KEY (ID)
+) ENGINE=Federated  ;
 
 -- Create a table for storing logs. For now we don't need to save them, but we need to implement functionality (Columns: ID, Time, LogRecord)
-
+CREATE TABLE Logs (
+    ID INT,
+    Timestampі Date,
+    Message VARCHAR(255),
+    PRIMARY KEY (ID)
+) ENGINE=MyISAM ;
 -- Create a table for storing reporting data, which will be send to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
+CREATE TABLE ProductReporting (
+    Date,
+    ProductName VARCHAR(50),
+    Orders VARCHAR(50)
+    PRIMARY KEY (ID)
+) ENGINE=CSV ;
