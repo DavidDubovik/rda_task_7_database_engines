99dd580fafc83f1dcbe94102f1caaae975dfab09
salodoxera <DavidDubovik19@gmail.com>
2025-01-07 19:47:03 +0200

    fix 0.4

--- task.sql
+++ task.sql
@@ -15,7 +15,7 @@ CREATE TABLE GeoIPCache (
     IPRange  INT,
     CountryID INT,
     PRIMARY KEY (ID)
-) ENGINE=InnoDB;
+) ENGINE=MEMORY;
 
 -- Create a table for storing product descriptions for different countries (Columns: ID, CountryID, ProductID, Description )
 CREATE TABLE ProductDescription (
@@ -29,10 +29,10 @@ CREATE TABLE ProductDescription (
 -- Create a table for storing logs. For now we don't need to save them, but we need to implement functionality (Columns: ID, Time, LogRecord)
 CREATE TABLE Logs (
     ID INT,
-    Timestamp DATE,
+    Timestamp DATETIME,
     Message VARCHAR(255),
     PRIMARY KEY (ID)
-) ENGINE=MyISAM ;
+) ENGINE=BLACKHOLE ;
 -- Create a table for storing reporting data, which will be send to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
 CREATE TABLE ProductReporting (
     Date DATE,
